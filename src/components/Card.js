d73958a1c55a5bcbf5df90a5712cddf431852fef
devop-mukul <mails2mukull@gmail.com>
2026-02-18 00:59:46 +0530

    Resolve merge conflicts

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ export default function Card() {
     <div>
         <div> 
             <div className = "card mt-3" style={{width: "18rem", "maxHeight": "360px"}}>
-                <img src="..." className = "card-img-top" alt="..."/>
+                <img src=".\burger.png" className = "card-img-top" alt="..."/>
                 <div className = "card-body">
                     <h5 className = "card-title">Card title</h5>
                     <p className = "card-text">Some important text.</p>
@@ -17,7 +17,7 @@ export default function Card() {
                                         <option key = {i + 1} value = {i + 1}> {i + 1}</option>
                                     )
                                 })
-                             }                    
+                             }
                         </select>
 
                         <select className = "m-2 h-100  bg-success rounded">
