149f6afa6b02d703cbb570b90e82f3994705fe2e
devop-mukul <mails2mukull@gmail.com>
2026-02-20 01:07:55 +0530

    made user schema

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = 'mongodb+srv://armukul01_db_user:Mukulmongodb11@clusterprojects.i9fd98w.mongodb.net/?appName=ClusterProjects'
+const mongoURI = "mongodb+srv://armukul01_db_user:Mukulmongodb11@clusterprojects.i9fd98w.mongodb.net/foodGoMERN?appName=ClusterProjects"
 
 const mongoDB = async() => {
     //     await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, result) => {
@@ -11,9 +11,14 @@ const mongoDB = async() => {
     try {
         await mongoose.connect(mongoURI);
         console.log("MongoDB Connected Successfully");
+        const fetchedData = await mongoose.connection.db
+            .collection("food_items")
+            .find({})
+            .toArray();
+        // console.log(fetchedData);
     } catch (error) {
         console.error("MongoDB Connection Failed:", error);
-        process.exit(1);
+        process.exit(1); // Exit the process with an error code
     }
 };
 
