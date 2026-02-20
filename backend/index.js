551229ccc1cadbb619e4412075a85f5f8615cae2
devop-mukul <mails2mukull@gmail.com>
2026-02-21 02:05:09 +0530

    Implement user signup form with validation and API integration, frontend requesto backend

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,13 @@ const port = 5000 //backend port
 const mongoDB = require('./db')
 mongoDB() //connect to mongoDB
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific HTTP methods
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Allow specific headers
+    next();
+})
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,27 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
+    const [formData, setFormData] = useState({name: "", email: "", password: "", location:""});
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        const response = await fetch('http://localhost:5000/api/createuser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+            // {name: formData.name, email: formData.email, password: formData.password, geoLocation: formData.geoLocation}
+        });
+
+        if(!response.ok){
+            throw new Error('Server error. Try again later.');
+        }
+
+        const json = await response.json();
+        console.log(json);
+
+        if(!json.success){
+            alert("Enter Valid Credentials");
+        }
+    }
+    const onChange = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
   return (
     <>
         <div className='container'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className = "mb-3">
-                    <label for="name" className = "form-label">Name</label>
-                    <input type="text" className = "form-control" id="name"/>
+                    <label htmlFor = "name" className = "form-label">Name</label>
+                    <input type="text" className = "form-control" name="name" value = {formData.name} onChange={onChange}/>
                 </div>
                 <div className = "mb-3">
-                    <label for="exampleInputEmail1" className = "form-label">Email address</label>
-                    <input type="email" className = "form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <label htmlFor = "exampleInputEmail1" className = "form-label">Email address</label>
+                    <input type="email" className = "form-control" name="email" value = {formData.email} onChange={onChange}/>
                     <div id="emailHelp" className = "form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className = "mb-3">
-                    <label for="exampleInputPassword1" className = "form-label">Password</label>
-                    <input type="password" className = "form-control" id="exampleInputPassword1"/>
+                    <label htmlFor = "location" className = "form-label">Address</label>
+                    <input type="text" className = "form-control" name="location" value = {formData.location}  onChange={onChange}/>
                 </div>
-                <button type="submit" className = "btn btn-primary">Submit</button>
+                
+                <div className = "mb-3">
+                    <label htmlFor = "exampleInputPassword1" className = "form-label">Password</label>
+                    <input type="password" className = "form-control" name="password" value = {formData.password} onChange={onChange}/>
+                </div>
+                <button type="submit" className = "m-3 btn btn-success">Submit</button>
+                {/* changed to btn-success from btn-primary */}
+                <Link to='/login' className='btn btn-danger'>Already a User!</Link>
             </form>
         </div>
     </>
   )
 }
-
